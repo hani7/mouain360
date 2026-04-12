2690ec67f03d387c37deefc69b0a558ea677d75c
hani7 <h.informatique7@gmail.com>
2026-04-12 12:54:25 +0100

    Add crash reporting to passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,16 @@
 import os
 import sys
-
-from django.core.wsgi import get_wsgi_application
+import traceback
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
-
-application = get_wsgi_application()
+def application(environ, start_response):
+    try:
+        from django.core.wsgi import get_wsgi_application
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+        _app = get_wsgi_application()
+        return _app(environ, start_response)
+    except Exception as e:
+        start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
+        error_msg = traceback.format_exc()
+        return [b"CRASH REPORT:\n\n", error_msg.encode('utf-8')]
