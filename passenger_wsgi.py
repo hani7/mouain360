1730003500f1f14b3c3e67aee8e19f1892ae783d
hani7 <h.informatique7@gmail.com>
2026-04-12 12:26:57 +0100

    Use absolute paths in passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,11 +3,8 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-# Add project directory to the sys.path
-cwd = os.getcwd()
-sys.path.append(cwd)
+sys.path.insert(0, os.path.dirname(__file__))
 
-# Point to your django application module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 application = get_wsgi_application()
